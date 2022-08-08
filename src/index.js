364b47acf6a2bb1cb8cd6241b9e7632897635e0f
Serhii <khanas.serhii@gmail.com>
2022-08-08 13:58:13 +0300

    minor change

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<ThemeProvider basename="/goit-react-hw-05-movies/" theme={theme}>
+		<BrowserRouter basename="/goit-react-hw-05-movies/">
+			<ThemeProvider theme={theme}>
 				<App />
 			</ThemeProvider>
 		</BrowserRouter>
